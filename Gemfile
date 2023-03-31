8fc022e50508cd0d6b1925aa3f094a1e7c8895fa
Vladislav Andreev <andreev@vlad.social>
2023-03-31 18:27:09 +0300

    Add views, add view-related plugins, setup layout
    - Add render to router
    - Add tilt and erubi gems
    - Add views and routes for root (home) and articles
    - Add navbar and plugin 'partial'
    - Refactor navbar partial, add link_to, partial plugins

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@
 source "https://rubygems.org"
 
 gem "dry-configurable", "~> 1.0"
+gem "erubi", "~> 1.12"
 gem "listen", "~> 3.8"
 gem "puma", "~> 6.2"
 gem "rake", "~> 13.0"
 gem "roda", "~> 3.66"
+gem "tilt", "~> 2.1"
--- app/router.rb
+++ app/router.rb
@@ -3,9 +3,23 @@
 require "roda"
 
 class Router < Roda
+  plugin :render, views: ApplicationSettings.root.join("app/views")
+  plugin :partials
+  plugin :link_to
+  plugin :path
+
+  path :root, "/"
+  path :articles, "/articles"
+
   route do |r|
     r.root do
-      "Hello, world!"
+      view "home"
+    end
+
+    r.on "articles" do
+      r.get do
+        view "articles/index"
+      end
     end
   end
 end
